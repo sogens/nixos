2dd65b8a20e26a0a7f7083befa6587523fdb435a
Jason Fischmann <fischmann.jason@gmail.com>
2022-08-02 18:04:33 +1000

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -18,11 +18,72 @@ in
 {
   imports =
     [ # Include the results of the hardware scan.
-
+       ./pulseeffects.nix
       ./hardware-configuration.nix
     ];
+####################################################################
+environment.localBinInPath = true;
+environment.variables = {
+    BROWSER = "google-chrome-stable";
+    TERM = "xterm-256color";
+    };
+ fonts = {
+    fontconfig.enable = true;
+    enableFontDir = true;
+    enableGhostscriptFonts = true;
+    };
+ networking.firewall.enable = true;
+  services.dbus.enable = true;
+#####################################################################
+fileSystems."/".options = [ "compress=zstd" "rw" "relatime" "ssd" "space_cache=v2" ];
+hardware.cpu.intel.updateMicrocode = true;
+services.timesyncd.enable = true;
+ programs.java.enable = true;
+   hardware.opengl.driSupport32Bit = true;
+  hardware.opengl.extraPackages32 = with pkgs.pkgsi686Linux; [ libva ];
+  hardware.enableAllFirmware = true;
+  hardware.enableRedistributableFirmware = true;
+   services.fwupd.enable = true;
+     services.smartd = {
+    enable = true;
+    # Monitor all devices connected to the machine at the time it's being started
+    autodetect = true;
+    notifications = {
+      x11.enable = if config.services.xserver.enable then true else false;
+      wall.enable = true; # send wall notifications to all users
+    };
+  };
+#####################################################################
+
+nix = {
+    trustedUsers = [ "root" "farlion" "@wheel" "@sudo" ];
+       binaryCachePublicKeys = [
+      "workflow.cachix.org-1:HhfBXgXCafJxYuATcMDQbC1qsbjF9qJUCchzFZS2zL4="
+      "jupyterwith.cachix.org-1:/kDy2B6YEhXGJuNguG1qyqIodMyO4w8KwWH4/vAc7CI="
+      "crazazy.cachix.org-1:3KaIHK26pkvd5palJH5A4Re1Hn2+GDV+aXYnftMYAm4=" # my own cache
+      "emacsng.cachix.org-1:i7wOr4YpdRpWWtShI8bT6V7lOTnPeI7Ho6HaZegFWMI=" # emacs-ng cache
+      "ethancedwards8.cachix.org-1:YMasjqyFnDreRQ9GXmnPIshT3tYyFHE2lUiNhbyIxOc=" # guix cache (for testing)
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" # mostly emacs-overlay
+      "nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4=" # flake shit
+      "rycee.cachix.org-1:TiiXyeSk0iRlzlys4c7HiXLkP3idRf20oQ/roEUAh/A=" # for updating my firefox plugins
+    ];
+    binaryCaches = [
+      "https://cache.nixos.org/"
+      "https://workflow.cachix.org/"
+      "https://jupyterwith.cachix.org/"
+       "https://crazazy.cachix.org"
+      "https://emacsng.cachix.org"
+      "https://ethancedwards8.cachix.org"
+      "https://nix-community.cachix.org"
+      "https://nrdxp.cachix.org"
+      "https://rycee.cachix.org"
+    ];
+};
+
 #####################################################################
     boot.loader.grub.enable = true;
+    boot.plymouth.enable = true;
+    boot.cleanTmpDir = true;
     boot.loader.efi.canTouchEfiVariables = true;
     boot.loader.grub.version = 2;
     boot.loader.grub.device = "nodev";
@@ -42,9 +103,27 @@ in
 		echo	'Loading initial ramdisk ...'
 		initrd	/early_ucode.cpio /initramfs-5.15.52-gentoo-x86_64.img
 }'';
+######################################################################
+
+i18n.inputMethod = {
+  enabled = "ibus";
+  ibus.engines = with pkgs.ibus-engines; [ anthy hangul mozc ];
+ };
+
+nixpkgs.config.permittedInsecurePackages = [
+                "electron-9.4.4"
+              ];
+
+######################################################################
+
+ security.polkit.enable = true;
+ xdg.portal.extraPortals = [pkgs.xdg-desktop-portal-wlr];
+ # services.xserver.displayManager.autoLogin.enable = true;
+ # services.xserver.displayManager.autoLogin.user = "jason";
 
 ######################################################################
    networking.hostName = "nixos";
+   networking.hostId = "4e13cd4c";
     networking.networkmanager.enable = true;
 
     time.timeZone = "Australia/Brisbane";
@@ -54,7 +133,17 @@ in
      font = "Lat2-Terminus16";
      keyMap = "us";
    };
-
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "en_AU.UTF-8";
+    LC_IDENTIFICATION = "en_AU.UTF-8";
+    LC_MEASUREMENT = "en_AU.UTF-8";
+    LC_MONETARY = "en_AU.UTF-8";
+    LC_NAME = "en_AU.UTF-8";
+    LC_NUMERIC = "en_AU.UTF-8";
+    LC_PAPER = "en_AU.UTF-8";
+    LC_TELEPHONE = "en_AU.UTF-8";
+    LC_TIME = "en_AU.UTF-8";
+  };
 nixpkgs.config = {
     allowUnfree = true;
     # more stuff
@@ -88,6 +177,11 @@ hardware.nvidia = {
 ###########################################
 services.flatpak.enable = true;
 xdg.portal.enable = true;
+
+############################################
+   virtualisation.virtualbox.host.enable = true;
+   users.extraGroups.vboxusers.members = [ "user-with-access-to-virtualbox" ];
+
 ############################################
 
 nix = {
@@ -102,7 +196,7 @@ extraOptions = "experimental-features = nix-command flakes";
   services.tumbler.enable = true;
    users.users.jason = {
      isNormalUser = true;
-     extraGroups = [ "wheel" ]; 
+     extraGroups = [ "wheel" "video" "audio" "disk" "networkmanager" ]; 
      packages = with pkgs; [
        firefox
        thunderbird
@@ -115,22 +209,21 @@ extraOptions = "experimental-features = nix-command flakes";
 	file
 	fish
 	tmux
-	neovim
-	ibus
 	kitty
 	curl
 	megasync
-	git
 	protonmail-bridge
-	gnome-menus
 	libreoffice
+	authy
      ]; 
    };  
 environment.systemPackages = with pkgs; [
-    wget vim nano zsh file fish tmux google-chrome neovim ibus kitty curl nvidia-offload gnome-menus discord
+    wget vim nano zsh file fish tmux google-chrome kitty curl nvidia-offload gnome-menus discord nodejs openjdk sqlite  stack haskell-language-server nushell nixpkgs-lint nixpkgs-fmt 
+    nixfmt emacs notify-desktop nnn nq fff trash-cli fzf surf unclutter gtk3-x11 gtk3 zlib php phpPackages.composer clang-tools gnumake binutils automake pciutils lm_sensors p7zip
+    unzip zip tor-browser-bundle-bin gnome.gnome-tweaks gparted pulseeffects-legacy gnomeExtensions.sound-output-device-chooser gnomeExtensions.dash-to-dock-for-cosmic gcc
 ];
 
- 
+system.autoUpgrade.enable = true; 
 ##################################################################################
 services.xserver.displayManager.gdm.enable = true;
 services.xserver.desktopManager.gnome.enable = true;
@@ -160,20 +253,19 @@ hardware = {
     enable = true;
     support32Bit = true;
     package = pkgs.pulseaudioFull;
+    daemon.config = {
+    default-sample-rate = 48000;
+    };
   };
 };
 ##################################################################################
+networking.enableIPv6 = false;
+programs.nix-ld.enable = true;
+security.sudo.wheelNeedsPassword = false;
+services.locate.enable = true;
 
 ###################################################################################
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   wget
-  # ];
 
-  # Some programs need SUID wrappers, can be configured further or are
-  #  started in user sessions.
     programs.mtr.enable = true;
     programs.gnupg.agent = {
       enable = true;
