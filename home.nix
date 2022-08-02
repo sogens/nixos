37e110897a7675b9257691e90a4fc88d0919f506
Jason Fischmann <fischmann.jason@gmail.com>
2022-08-02 18:06:59 +1000

    Add files via upload

--- home.nix
+++ home.nix
@@ -15,14 +15,34 @@ home.sessionVariables.HOSTNAME = "nixos";
     };
    };
      programs.home-manager.enable = true;
+     programs.dircolors.enable = true;
+     programs.gpg.enable = true;
+     programs.fzf.enable = true;
      modules.editors.neovim.enable = true;
      modules.editors.vscode.enable = true;
      modules.shell.base.enable = true;
      modules.shell.zsh.enable = true;
-     modules.desktop.kitty.enable = true;
+     modules.desktop.kitty.enable = true; 
 
-     home.packages = with pkgs; [ fd ripgrep fortune cowsay go luarocks neovim-nightly python3 python cargo xclip openjdk ];
-     
+     home.packages = with pkgs; [ fd ripgrep fortune cowsay go luarocks neovim python3 python cargo xclip zplug lf alacritty spotifyd spotify-tui ];
+    
+     programs.zsh.zplug = {
+      enable = true;
+      plugins = [
+        { name = "zsh-users/zsh-autosuggestions"; }
+        { name =  "ohmyzsh/ohmyzsh oh-my-zsh"; }
+        { name =  "ajeetdsouza/zoxide"; }
+        { name =  "marlonrichert/zsh-autocomplete"; }
+        { name =  "marlonrichert/zsh-edit"; }
+        { name =  "zsh-users/zsh-autosuggestions"; }
+        { name =  "ptavares/zsh-exa"; }
+        { name =  "zsh-users/zsh-syntax-highlighting"; }
+        { name =  "mbenford/zsh-tmux-auto-title"; }
+        { name =  "zsh-users/zsh-history-substring-search"; }
+      ];
+    };
+
+
     programs.git = {
     package = pkgs.gitAndTools.gitFull;
     enable = true;
